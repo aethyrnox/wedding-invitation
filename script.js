e196534daeecc25e1721aafa2d2033a26c13a07a
Rama Rahmat <ramarahmat41@gmail.com>
2025-07-09 10:43:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -33,5 +33,31 @@ function createPetal() {
     petal.remove();
   }, 5000);
 }
-
 setInterval(createPetal, 500);
+window.addEventListener("load", function () {
+  new Glider(document.querySelector('.glider'), {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: '.dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    },
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
